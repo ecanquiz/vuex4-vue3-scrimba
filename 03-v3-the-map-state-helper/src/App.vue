a887fecdbeaa6928eb711bdf5a4b6c0625169957
ejimenez123 <caribestic123@gmail.com>
2021-10-12 18:47:33 -0400

    feat: Lesson 4 - The mapState Helper

--- 03-v3-the-map-state-helper/src/App.vue
+++ 03-v3-the-map-state-helper/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    {{ count }}
+    {{ countPlusLocalState() }}
   </div>
 </template>
 
@@ -8,8 +8,8 @@
   import { ref, computed } from 'vue'
   import { useStore } from "vuex";
 
-  const store = useStore()  
-  const count = computed(() => store.state.count)    
+  const store = useStore()
+  const localCount = ref(1)
+  const count = computed(() => store.state.count)  
+  const countPlusLocalState = () => count.value + localCount.value  
 </script>
-
-
