5350865998d211ae64b4851ffb967130055c4835
ejimenez123 <caribestic123@gmail.com>
2021-10-18 17:25:05 -0400

    feat: Lesson 07 - Modules with script-setup

---
+++ 07-v3-modules/src/store/moduleB.js
@@ -0,0 +1,13 @@
+import subModule from './subModule'
+export default {
+  namespaced: true,
+  modules: {
+    subModule
+  },
+  state: () => ({
+    count: 8
+  }),
+  mutations: { },
+  getters: { },
+  actions: { }
+}
