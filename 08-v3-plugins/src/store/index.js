e56a1d9a77fd31012433d856a820341bb9427ed7
ejimenez123 <caribestic123@gmail.com>
2021-10-20 19:42:07 -0400

    feat: Lesson 08 - Plugins with Option API

---
+++ 08-v3-plugins/src/store/index.js
@@ -0,0 +1,41 @@
+import { createStore, createLogger } from 'vuex'
+import moduleA from './moduleA'
+import moduleB from './moduleB'
+import moduleC from './moduleC'
+
+export default createStore({
+  plugins: process.env.NODE_ENV !== 'production'
+    ? [createLogger()] : [],
+  modules: {
+    moduleA,
+    moduleB,    
+    moduleC
+  },
+  state: () => ({
+    count: 2
+  }),
+  mutations: {
+    increment (state) {
+      state.count++
+    },
+    decrement (state) {
+      state.count--
+    }
+  },  
+  actions: {
+    asyncIncrement ({dispatch}) {
+      setTimeout(() => {
+        dispatch('someOtherAction')
+      }, 1000)
+    },
+    someOtherAction ({commit}) {    
+      commit('increment')
+    }
+  },
+  getters: {  
+    tripleCount (state) {
+      return state.count * 3
+    }  
+  }
+});
+
