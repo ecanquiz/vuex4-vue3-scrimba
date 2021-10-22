c4b397152ec4483de7bb329f2bdf32f9c47a3527
Oswaldo Graterol <oswaldo.graterol@gmail.com>
2021-10-22 02:27:09 -0400

    feat: Lesson 4 - Getters with Script setup

--- 04-v3-getters/src/App.vue
+++ 04-v3-getters/src/App.vue
@@ -1,19 +1,27 @@
 <template>
   <div>
-    {{ todos }}
-    <!-- Completed Todos: {{ doneTodosCount }} -->
-    <!--ul>
-      <li v-for="todo in todos" :key="todo.id" >{{todo.text}} | {{todo.done}}</li>
-    </ul-->
+    {{ doneTodos }}
+    <br />
+    Completed Todos: {{ doneTodosCount }}
+    <br />
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.text }} | {{ todo.done }}
+      </li>
+    </ul>
+    <br />
+    Todos by ID {{ id }}: {{ getTodosById }}
   </div>
 </template>
 
 <script setup>
-  import { ref, computed } from 'vue'
-  import { useStore } from "vuex";
+import { ref, computed } from "vue";
+import { useStore } from "vuex";
 
-  const store = useStore()  
-  const todos = computed(() => store.state.todos)    
+const store = useStore();
+const todos = computed(() => store.state.todos);
+const doneTodos = computed(() => store.getters.doneTodos);
+const doneTodosCount = computed(() => store.getters.doneTodosCount);
+const id = ref(2);
+const getTodosById = computed(() => store.getters.getTodoById(id.value));
 </script>
-
-
--- 04-v3-getters/src/main.js
+++ 04-v3-getters/src/main.js
@@ -1,17 +1,25 @@
-import { createApp } from 'vue'
-import { createStore } from 'vuex'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createStore } from "vuex";
+import App from "./App.vue";
 
 const store = createStore({
   state: {
-       todos: [
-           { id: 1, text: '...', done: true },
-           { id: 2, text: '...', done: false },
-       ]
-  }    
-})
+    todos: [
+      { id: 1, text: "...", done: true },
+      { id: 2, text: "...", done: false },
+    ],
+  },
+  getters: {
+    doneTodos: (state) => {
+      return state.todos.filter((todo) => todo.done);
+    },
+    doneTodosCount: (state, getters) => {
+      return getters.doneTodos.length;
+    },
+    getTodoById: (state) => (id) => {
+      return state.todos.find((todo) => todo.id == id);
+    },
+  },
+});
 
-createApp(App)
-  .use(store)
-  .mount('#app')
-
+createApp(App).use(store).mount("#app");
