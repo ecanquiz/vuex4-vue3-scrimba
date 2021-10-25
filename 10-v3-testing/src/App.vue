a682c31e181f3497776a93ea5873626e9907db91
ejimenez123 <caribestic123@gmail.com>
2021-10-25 18:02:45 -0400

    feat: Lesson 10 - Testing with Option API

--- 10-v3-testing/src/App.vue
+++ 10-v3-testing/src/App.vue
@@ -1,8 +1,24 @@
 <template>
-  <div>    
+  <div>
+    <p>Count: {{ count }}</p>
+    <button @click='increment' id="increment">+</button>
+    <button @click='incrementAsync' id="incrementAsync">+ (async)</button>
+    <p>Products: {{ products }}</p>    
+    <p>Filtered Products: {{ filteredProducts({filterCategory: 'fruit'}) }}</p>
   </div>
 </template>
 
-<script setup>
+<script>
+import { mapState, mapActions, mapMutations, mapGetters } from 'vuex'
 
+export default {
+  computed: {
+    ...mapState(['count', 'products']),
+    ...mapGetters(['filteredProducts'])       
+  },
+  methods: {
+    ...mapMutations(['increment']),
+    ...mapActions(['incrementAsync'])
+  }
+};
 </script>
