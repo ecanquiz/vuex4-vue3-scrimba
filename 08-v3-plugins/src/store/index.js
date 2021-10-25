b1f8986c67cb6501d98a01a8dd4a541608fc9bee
ejimenez123 <caribestic123@gmail.com>
2021-10-25 15:01:00 -0400

    feat: Lesson 08 - Plugins with Option API

--- 08-v3-plugins/src/store/index.js
+++ 08-v3-plugins/src/store/index.js
@@ -1,42 +1,48 @@
 import { createStore, createLogger } from 'vuex'
-import moduleA from './moduleA'
-import moduleB from './moduleB'
-import moduleC from './moduleC'
+import * as moduleA from "./moduleA";
+import * as moduleB from "./moduleB";
+import * as moduleC from "./moduleC";
+
+export const state = ()=> ({
+  count: 2
+});
+
+export const mutations = {
+  increment (state) {
+    state.count++
+  },
+  decrement (state) {
+    state.count--
+  }
+};
+
+export const actions = {
+  asyncIncrement ({dispatch}) {
+    setTimeout(() => {
+      dispatch('someOtherAction')
+    }, 1000)
+  },
+  someOtherAction ({commit}) {    
+    commit('increment')
+  }
+};
+
+export const getters = {
+  tripleCount (state) {
+    return state.count * 3
+  }  
+};
 
 export default createStore({
   strict: process.env.NODE_ENV !== 'production',
-  plugins: process.env.NODE_ENV !== 'production'
-    ? [createLogger()] : [],
+  plugins: process.env.NODE_ENV !== 'production' ? [createLogger()] : [],
   modules: {
     moduleA,
     moduleB,    
     moduleC
   },
-  state: () => ({
-    count: 2
-  }),
-  mutations: {
-    increment (state) {
-      state.count++
-    },
-    decrement (state) {
-      state.count--
-    }
-  },  
-  actions: {
-    asyncIncrement ({dispatch}) {
-      setTimeout(() => {
-        dispatch('someOtherAction')
-      }, 1000)
-    },
-    someOtherAction ({commit}) {    
-      commit('increment')
-    }
-  },
-  getters: {  
-    tripleCount (state) {
-      return state.count * 3
-    }  
-  }
+  state,
+  mutations,
+  actions,
+  getters
 });
-
