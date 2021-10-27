b39b13183a54afd6dd9d0471471dff46d6e96b5e
Oswaldo Graterol <oswaldo.graterol@gmail.com>
2021-10-27 10:50:04 -0400

    feat: Lesson 4 - Getters with script-setup

--- 04-v3-getters/src/main.js
+++ 04-v3-getters/src/main.js
@@ -1,25 +1,5 @@
 import { createApp } from "vue";
-import { createStore } from "vuex";
+import { store } from "./store";
 import App from "./App.vue";
-
-const store = createStore({
-  state: {
-    todos: [
-      { id: 1, text: "...", done: true },
-      { id: 2, text: "...", done: false },
-    ],
-  },
-  getters: {
-    doneTodos: (state) => {
-      return state.todos.filter((todo) => todo.done);
-    },
-    doneTodosCount: (state, getters) => {
-      return getters.doneTodos.length;
-    },
-    getTodoById: (state) => (id) => {
-      return state.todos.find((todo) => todo.id == id);
-    },
-  },
-});
 
 createApp(App).use(store).mount("#app");
---
+++ 04-v3-getters/src/store.js
@@ -0,0 +1,21 @@
+import { createStore } from "vuex";
+
+export const store = createStore({
+  state: {
+    todos: [
+      { id: 1, text: "...", done: true },
+      { id: 2, text: "...", done: false },
+    ],
+  },
+  getters: {
+    doneTodos: (state) => {
+      return state.todos.filter((todo) => todo.done);
+    },
+    doneTodosCount: (state, getters) => {
+      return getters.doneTodos.length;
+    },
+    getTodoById: (state) => (id) => {
+      return state.todos.find((todo) => todo.id == id);
+    },
+  },
+});
