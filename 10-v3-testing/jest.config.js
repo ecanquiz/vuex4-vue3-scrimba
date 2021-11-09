e36276c2986171121a349b2a06314da0dc1025bd
Oswaldo Graterol <oswaldo.graterol@gmail.com>
2021-11-09 10:51:47 -0400

    feat: Lesson 10 - Testing with script-setup

---
+++ 10-v3-testing/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  transformIgnorePatterns: ["/node_modules/"],
+  moduleFileExtensions: ["js", "jsx", "json", "vue"],
+  transform: {
+    ".*\\.(vue)$": "vue-jest",
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+  },
+};
