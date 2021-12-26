e11d58392aaa5cfca82e38858960d7cb66684383
ejimenez123 <caribestic123@gmail.com>
2021-12-26 10:21:26 -0500

    feat: Lesson 06 - Actions with script-setup

--- 06-v3-actions/src/App.vue
+++ 06-v3-actions/src/App.vue
@@ -1,18 +1,21 @@
+<script setup>
+import { ref, computed } from 'vue'
+import { useStore } from "vuex";
+  
+const store = useStore()  
+const count = computed(() => store.state.count)
+const increment = () => store.dispatch('incrementAsync')
+const decrement = () => store.commit('decrement')
+const actionB = () => store.dispatch('actionB')
+const actionD = () => store.dispatch('actionD')
+</script>
+
 <template>
   <div>
     {{ count }}
-    <button @click="increment">+</button>
-    <button @click="decrement">-</button>
+    <button @click='increment'>+</button>
+    <button @click='decrement'>-</button>
+    <button @click='actionB'>actionB</button>
+    <button @click='actionD'>actionD</button>
   </div>
 </template>
-
-<script setup>
-import { ref, computed } from "vue";
-import { useStore } from "vuex";
-
-const store = useStore();
-const count = computed(() => store.getters.getCount);
-
-const increment = () => store.dispatch("increment");
-const decrement = () => store.dispatch("decrement");
-</script>
--- 06-v3-actions/src/store.js
+++ 06-v3-actions/src/store.js
@@ -1,30 +1,55 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex'
+import { getData, getOtherData } from './data'
 
 export default createStore({
   state: {
-    count: 0,
-  },
-  getters: {
-    getCount(state) {
-      return state.count;
-    },
+    count: 0
   },
   mutations: {
-    increment(state) {
-      state.count++;
+    increment (state) {
+      state.count++
     },
-    decrement(state) {
-      state.count--;
+    decrement (state) {
+      state.count--
     },
+    mutationA (state) {
+      state.count += 10
+    },
+    mutationB (state) {
+      state.count += 100
+    },
+    mutationC (state, ten) {      
+      state.count -= ten
+    },
+    mutationD (state, hundred) {      
+      state.count -= hundred
+    }
   },
   actions: {
-    //context object
-    increment(context) {
-      context.commit("increment");
+    incrementAsync ({ commit }) {
+      setTimeout(() => {
+        commit('increment')
+      }, 1000)
     },
-    //ES2015 argument destructuring
-    decrement({ commit }) {
-      commit("decrement");
+    actionA ({ commit }) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          commit('mutationA')
+            resolve()
+        }, 1000)
+      })
     },
-  },
+    actionB ({ dispatch, commit }) {
+      return dispatch('actionA').then(() => {
+        commit('mutationB')
+      })
+    },
+    async actionC ({ commit }) {      
+      commit('mutationC', await getData())
+    },
+    async actionD ({ dispatch, commit} ) {
+      await dispatch('actionC')
+      commit('mutationD', await getOtherData())
+    }
+  }
 });
