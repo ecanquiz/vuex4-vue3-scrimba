498e01dccca04578c8433bbe451e902e0536e4cc
ejimenez123 <caribestic123@gmail.com>
2021-12-25 19:29:27 -0500

    feat: Lesson 01 - Getting Started with script-setup

--- 01-v3-getting-started/src/App.vue
+++ 01-v3-getting-started/src/App.vue
@@ -4,25 +4,17 @@
   </div>
 </template>
 
-<script>
-import { ref, onMounted } from "vue";
-import { useStore } from "vuex";
+<script setup>
+  import { ref, onMounted } from "vue";
+  import { useStore } from 'vuex'
 
-export default {
-  setup() {
+  const message = ref('Hello world')
+  const store = useStore()  
 
-    const store = useStore();
-    const message = ref("Hello world");
-    
-    const increment = () => {
-        store.commit("increment");
-        console.log(store.state.count);
-      };
+  const increment = ()=> {
+    store.commit('increment')
+    console.log(store.state.count)
+  }  
 
-    onMounted(() => increment());
-
-    return { message, increment };
-    
-  }
-};
+  onMounted(() => increment());
 </script>
