efff71853f3f16db0ebb35f63377812f85f9feca
ejimenez123 <caribestic123@gmail.com>
2021-12-26 07:34:27 -0500

    feat: Lesson 02 - Getting Vuex State into Vue Components with script-setup

--- 02-v3-single-state-tree/src/App.vue
+++ 02-v3-single-state-tree/src/App.vue
@@ -1,20 +1,13 @@
+<script setup>
+import { computed } from "vue"
+import { useStore } from "vuex"
+
+const store = useStore()
+const count = computed(() => store.state.count)
+</script>
+
 <template>
   <div>
     {{ count }}
   </div>
 </template>
-
-<script>
-import { computed } from 'vue'
-import { useStore } from "vuex";
-
-export default {
-  setup() {      
-    const store = useStore();
-
-    return {
-      count: computed(() => store.state.count)
-    };      
-  }
-};  
-</script>
